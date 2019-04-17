95e7d83c6eb5e3c58743da3fc27a0c29a84bb7ef
AnteusNar <jedidiah.h.wallace@gmail.com>
2019-04-16 18:29:20 -0700

    starting with variables

---
+++ intro.js
@@ -0,0 +1,22 @@
+var num = 1.345
+var str = 'fish'
+var bln = true
+
+var nil = null
+var und
+
+var arr = [1,2,3,'str'] //arry
+
+var odj = {
+    age: 32,
+    hairColor:'red',
+    foo:{
+    bar: 'bat'
+    }
+}
+
+obj.foo.bar
+obj['foo']['bar']
+obj['age']
+
+function
